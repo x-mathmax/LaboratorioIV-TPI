93c0f688418fc9990c98739c1e241f484287642c
x-mathmax <barbaramiamolinari@gmail.com>
2024-04-17 01:02:44 -0300

    login visual ok

---
+++ src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+
+
+export const routes: Routes = [
+    { path: '', redirectTo: '/login', pathMatch: "full" },
+    { path: 'login', component: LoginComponent },
+    { path: '**', component: PageNotFoundComponent }
+];
---
+++ src/models/usuario.model.ts
@@ -0,0 +1,9 @@
+export class Usuario {
+    nombre: string;
+    clave: string;
+  
+    constructor(nombre: string, clave: string,) {
+      this.nombre = nombre;
+      this.clave = clave;
+    }
+  }
