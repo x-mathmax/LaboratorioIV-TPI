1427c2ae54d61563199bfe50913f259c47269ff4
x-mathmax <barbaramiamolinari@gmail.com>
2024-04-21 19:12:46 -0300

    modifico ruteos internos

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,7 +14,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'claseDos' title`, () => {
+  it(`should have the 'LaboratorioIV-TPI' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('Sala de Juegos');
